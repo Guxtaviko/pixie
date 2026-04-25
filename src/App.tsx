561194f1ba63e8e33607bc97c15b7c60a0d43fa1
Gustavo Vieira <guxtaviko@hotmail.com>
2026-04-25 16:10:29 -0300

    fix: ensure toggleSidebar is passed to header

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export const App = () => {
 
 	return (
 		<div className='min-h-dvh bg-radial bg-slate-50 dark:bg-slate-950 text-slate-800 dark:text-slate-200  overflow-hidden selection:bg-cyan-500/50 selection:text-white antialiased duration-300'>
-			<Header onToggleSidebar={isMobile ? toggleSidebar : undefined} />
+			<Header onToggleSidebar={toggleSidebar} />
 			<main className='flex h-[calc(100dvh-4rem)] md:h-[calc(100vh-4rem)] relative flex-col md:flex-row'>
 				<ToolBar />
 				<Canvas />
